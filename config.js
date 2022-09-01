f2794f959c512fc2af0f7747f95de27dd153381e
egmu <mkh.yusuf@gmail.com>
2022-09-01 22:59:27 +0700

    Bot WA V10

---
+++ config.js
@@ -0,0 +1,60 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapi.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapi.xyz': 'sonelstore',
+}
+
+//api
+global.xteam = 'apivproject'
+global.lolhuman = 'a957f329d271e062a74f724d'
+
+// Other
+global.owner = ['6289519240209']
+global.premium = ['6289519240209']
+global.ownername = '🆁🆉🅻01'
+global.botname = '🅂🄸🄰🄿🄰🄱🄾🅃√'
+global.packname = '© 🆁🆉🅻01'
+global.gc = 'https://github.com/egmu'
+global.linkyt = 'https://youtube.com/c/DEVIL-X-CODE'
+global.linkgc = 'https://chat.whatsapp.com/G8OUIwUS5vR8tgWZ4bpMXn'
+global.limitawal = '10'
+global.author = '@🆁🆉🅻01\n@🅂🄸🄰🄿🄰🄱🄾🅃√'
+global.sessionName = '🅂🄸🄰🄿🄰🄱🄾🅃'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⌕'
+global.mess = {
+    success: 'Berhasil',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Tunggu sebentar, sedang di proses',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.sc = fs.readFileSync('./media/sc.jpg')
+global.tq = fs.readFileSync('./media/tq.jpg')
+global.menu = fs.readFileSync('./media/menu.jpg')
+global.allmenu = fs.readFileSync('./media/allmenu.jpg')
+global.thumb = { url: 'https://i.pinimg.com/736x/d0/f8/b8/d0f8b804a908ce4aaee63d54035d2192.jpg' }
+global.visoka = { url: 'https://telegra.ph/file/769d88bb8c8357546a149.mp4' }
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
